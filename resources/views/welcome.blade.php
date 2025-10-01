1da8fc84e0665a80c2b41e50b3b15e6f37e88a84
mohamad azdi <azdi@um.edu.my>
2025-10-01 12:47:54 +0800

    updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
